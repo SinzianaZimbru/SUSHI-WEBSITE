f2193d90311a95e6130f156e3b8168c4e92abaab
Sinziana <zimbru.sinziana@yahoo.com>
2020-06-04 21:54:45 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+
+// TESTIMONIALS
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:5000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+ // COUNT NUMBER
+ $('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 2000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+       
